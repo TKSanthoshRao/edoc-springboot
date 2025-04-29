1648f0a394595a6076933455604dea358835d2f8
T K Santhosh Rao <120514816+TKSanthoshRao@users.noreply.github.com>
2025-04-29 20:15:25 +0530

    Update ContactController.java

--- src/main/java/org/santhu/springbackend/controller/ContactController.java
+++ src/main/java/org/santhu/springbackend/controller/ContactController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 @RestController
 //@CrossOrigin(origins = "*")
+    @CrossOrigin(origins = "http://localhost:9090")
 @RequestMapping("/api")
 public class ContactController {
 
